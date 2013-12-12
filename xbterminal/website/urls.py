7ad0b41e21f2d88a8c8c5f95efbaeab210cfbf3c
Sazonov Aleksey <devnull@localhost>
2013-12-12 12:02:02 +0400

    switch contact link to contact form

--- xbterminal/website/urls.py
+++ xbterminal/website/urls.py
@@ -2,8 +2,8 @@ from django.conf.urls import patterns, url
 from website import views
 
 urlpatterns = patterns('',
-  url(r'^$', views.contact, name='contact'),
-  url(r'^landing/', views.landing, name='landing'),
-  url(r'^faq',views.landing_faq,name='landing_faq'),
+  url(r'^$', views.landing, name='landing'),
+  url(r'^contact/', views.contact, name='contact'),
+  url(r'^faq/',views.landing_faq,name='landing_faq'),
   url(r'^merchant/',views.merchant,name='merchant')
 )
