484742b46a2f6c7b4fe45468ab33b8c3eb7ebef6
Sazonov Aleksey <devnull@localhost>
2013-12-15 14:59:37 +0400

    try auth

--- xbterminal/website/urls.py
+++ xbterminal/website/urls.py
@@ -2,10 +2,10 @@ from django.conf.urls import patterns, url
 from website import views
 
 urlpatterns = patterns('',
+  url(r'^accounts/login/$','django.contrib.auth.views.login',{'template_name':'website/login.html'}),
   url(r'^$', views.landing, name='landing'),
   url(r'^contact/', views.contact, name='contact'),
   url(r'^faq/',views.landing_faq,name='landing_faq'),
   url(r'^merchant/',views.merchant,name='merchant'),
-  url(r'^login/$',views.merchant_login,name='login'),
-  url(r'^cabinet/$',views.merchant_cabinet,name='merchant_cabinet'),
+  url(r'^cabinet/',views.merchant_cabinet,name='merchant_cabinet'),
 )
