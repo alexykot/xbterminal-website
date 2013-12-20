80a699f6e730ac8cd478276d797d99f935c65316
Lenur Zamzaraev <zamzaraevl@gmail.com>
2013-12-20 16:34:36 +0200

    fixed login and register links when user is logged in

--- xbterminal/website/urls.py
+++ xbterminal/website/urls.py
@@ -1,14 +1,9 @@
 from django.conf.urls import patterns, url
 
 from website import views
-from website.forms import AuthenticationForm
 
 
 urlpatterns = patterns('',
-    url(r'^accounts/login/$', 'django.contrib.auth.views.login', {
-        'template_name': 'website/login.html',
-        'authentication_form': AuthenticationForm
-    }),
     url(r'^$', views.landing, name='landing'),
     url(r'^contact/', views.contact, name='contact'),
     url(r'^faq/', views.landing_faq, name='landing_faq'),
--- xbterminal/xbterminal/urls.py
+++ xbterminal/xbterminal/urls.py
@@ -1,12 +1,21 @@
 from django.conf.urls import patterns, include, url
-
 from django.contrib import admin
+
+from website.forms import AuthenticationForm
+
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'xbterminal.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'', include('website.urls',namespace='website')),
+    url(r'^login/$', 'django.contrib.auth.views.login', {
+            'template_name': 'website/login.html',
+            'authentication_form': AuthenticationForm
+        },
+        'login'),
+    url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': '/'}, name='logout'),
+    url(r'', include('website.urls', namespace='website')),
     url(r'^admin/', include(admin.site.urls)),
 )
