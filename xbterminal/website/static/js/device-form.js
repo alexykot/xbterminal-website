cbbb168ec4f1a6c967d3c45b38d89283734958d8
Lenur Zamzaraev <zamzaraevl@gmail.com>
2014-01-07 01:13:59 +0200

    fixed device-form.js

--- xbterminal/website/static/js/device-form.js
+++ xbterminal/website/static/js/device-form.js
@@ -3,6 +3,7 @@ $(document).ready(function(){
     var paymentProcessor = $('[name=payment_processor]');
     var percent = $('[name=percent]');
     var apiKey = $('[name=api_key]');
+    var bitcoinAddress = $('[name=bitcoin_address]');
 
     paymentProcessing.change(function(){
         if ($(this).val() == 'keep'){
@@ -11,18 +12,21 @@ $(document).ready(function(){
             paymentProcessor.parent().addClass('disabled').removeClass('active');
             percent.attr('disabled', true).val('');
             apiKey.attr('disabled', true);
+            bitcoinAddress.attr('disabled', false);
         }
         if ($(this).val() == 'partially'){
             paymentProcessor.attr('disabled', false);
             paymentProcessor.parent().removeClass('disabled');
             percent.attr('disabled', false);
             apiKey.attr('disabled', false);
+            bitcoinAddress.attr('disabled', false);
         }
         if ($(this).val() == 'full'){
             paymentProcessor.attr('disabled', false);
             percent.attr('disabled', true).val('100');
             paymentProcessor.parent().removeClass('disabled');
             apiKey.attr('disabled', false);
+            bitcoinAddress.attr('disabled', true);
         }
     });
     paymentProcessing.filter(':checked').trigger('change');
