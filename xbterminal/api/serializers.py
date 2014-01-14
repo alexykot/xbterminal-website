28b35ca8ebc81c12e1fc3e48e5ad2a2f1c381e5f
Lenur Zamzaraev <zamzaraevl@gmail.com>
2014-01-14 03:23:52 +0200

    changed device slug field

--- xbterminal/api/serializers.py
+++ xbterminal/api/serializers.py
@@ -5,6 +5,7 @@ from website.models import Transaction
 
 class TransactionSerializer(serializers.ModelSerializer):
     receipt_url = serializers.CharField(source='get_api_url', read_only=True)
+    device = serializers.SlugRelatedField(slug_field='key')
 
     class Meta:
         model = Transaction
