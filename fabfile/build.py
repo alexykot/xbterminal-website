1ffe88f3b86b6104aef3cca242828e800ed6d6fb
Kirill Goncharov <kdgoncharov@yandex.ru>
2014-05-06 22:29:11 +0400

    Fix; updated django to latest version

--- fabfile/build.py
+++ fabfile/build.py
@@ -6,7 +6,7 @@ def venv():
     with settings(warn_only=True):
         result = env.run("test requirements.txt -ot venv/bin/activate")
     if result.failed:
-        env.run("virtualenv venv")
+        env.run("virtualenv -p /usr/bin/python2 venv")
         with prefix(". venv/bin/activate"):
             env.run("pip install -r requirements.txt --upgrade --allow-all-external --allow-unverified pyPdf")
         env.run("touch venv/bin/activate")
