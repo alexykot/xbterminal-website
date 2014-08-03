1ae8ea4027a0596935a31d93077c2d29be3cd04a
Kirill Goncharov <kdgoncharov@yandex.ru>
2014-08-03 14:15:55 +0400

    Sticky header

--- xbterminal/website/static/js/landing.js
+++ xbterminal/website/static/js/landing.js
@@ -1,6 +1,34 @@
 var Landing = (function () {
     'use strict';
+    var makeLinkActive = function (sectionId) {
+        $('.header .menu-block a').each(function () {
+            var link = $(this);
+            if (link.attr('href') == '/#' + sectionId) {
+                link.addClass('active');
+            } else {
+                link.removeClass('active');
+            }
+        });
+    };
+
     var init = function () {
+        $(window).on('scroll', function () {
+            var navHeight = $('.header').height();
+            if ($(window).scrollTop() > navHeight) {
+                $('.header').addClass('fixed');
+            } else {
+                $('.header').removeClass('fixed');
+            }
+        });
+
+        var sections = $('#about-section, #terminal-section, \
+                          #mobile-section, #web-section');
+
+        sections.on('scrollSpy:enter', function () {
+            makeLinkActive($(this).attr('id'));
+        });
+        sections.scrollSpy();
+
     };
     return {init: init};
 }());
