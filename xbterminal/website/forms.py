3c849a429a7bc57037e1a8cf354d4fdae9ef2fef
Kirill Goncharov <kdgoncharov@yandex.ru>
2014-08-20 00:49:29 +0400

    Fixed device key validator

--- xbterminal/website/forms.py
+++ xbterminal/website/forms.py
@@ -299,7 +299,7 @@ class SendDailyReconciliationForm(forms.ModelForm):
 class EnterAmountForm(forms.Form):
 
     device_key = forms.CharField(
-        validators=[RegexValidator('^[0-9a-fA-F]{32}$')])
+        validators=[RegexValidator('^[0-9a-zA-Z]{8,32}$')])
     amount = forms.DecimalField(
         max_digits=9,
         decimal_places=2,
