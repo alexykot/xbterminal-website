96d41fb98be30ed4ed5915f9779c0a128cbd34d2
Kirill Goncharov <kdgoncharov@yandex.ru>
2014-08-19 20:41:11 +0400

    Added language prefixes to urls

--- xbterminal/xbterminal/urls.py
+++ xbterminal/xbterminal/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.conf.urls.i18n import i18n_patterns
 
 admin.autodiscover()
 
@@ -13,8 +14,11 @@ urlpatterns = patterns('',
         {'next_page': '/'},
         name='logout'),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'', include('website.urls', namespace='website')),
     url(r'^api/', include('api.urls', namespace='api')),
     url(r'^django-rq/', include('django_rq.urls')),
     url(r'^jsi18n/$', 'django.views.i18n.javascript_catalog', {'packages': ('xbterminal',)}),
+)
+
+urlpatterns += i18n_patterns('',
+    url(r'', include('website.urls', namespace='website')),
 )
