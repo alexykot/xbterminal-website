558270c84e74c24b1fe97175a5e65666f71b8880
Kirill Goncharov <kdgoncharov@yandex.ru>
2015-05-17 17:15:30 +0300

    Create factory for MerchantAccount

--- xbterminal/website/tests/factories.py
+++ xbterminal/website/tests/factories.py
@@ -1,6 +1,6 @@
 import factory
 
-from website.models import User
+from website.models import User, MerchantAccount
 
 
 class UserFactory(factory.DjangoModelFactory):
@@ -10,3 +10,23 @@ class UserFactory(factory.DjangoModelFactory):
 
     email = factory.Sequence(lambda n: 'user_{0}@xbterminal.io'.format(n))
     password = factory.PostGenerationMethodCall('set_password', 'password')
+
+
+class MerchantAccountFactory(factory.DjangoModelFactory):
+
+    class Meta:
+        model = MerchantAccount
+
+    user = factory.SubFactory(UserFactory)
+    company_name = factory.Sequence(lambda n: 'Company {0}'.format(n))
+    trading_name = factory.LazyAttribute(lambda ma: ma.company_name)
+
+    business_address = 'Test Address, 123'
+    town = 'London'
+    post_code = 'ABC 123'
+    country = 'GB'
+
+    contact_first_name = 'Test'
+    contact_last_name = 'Test'
+    contact_phone = '+123456789'
+    contact_email = factory.LazyAttribute(lambda ma: ma.user.email)
--- xbterminal/website/tests/test_models.py
+++ xbterminal/website/tests/test_models.py
@@ -1,12 +1,17 @@
 from django.test import TestCase
 
 from website.models import User
-from website.tests.factories import UserFactory
+from website.tests.factories import UserFactory, MerchantAccountFactory
 
 
 class UserTestCase(TestCase):
 
     def test_create_user(self):
+        user = User.objects.create(email='test@example.com')
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+
+    def test_user_factory(self):
         user = UserFactory.create()
         self.assertTrue(user.is_active)
         self.assertFalse(user.is_staff)
@@ -15,3 +20,15 @@ class UserTestCase(TestCase):
     def test_get_full_name(self):
         user = UserFactory.create()
         self.assertEqual(user.get_full_name(), user.email)
+
+
+class MerchantAccountTestCase(TestCase):
+
+    fixtures = ['initial_data.json']
+
+    def test_merchant_factory(self):
+        merchant = MerchantAccountFactory.create()
+        self.assertEqual(merchant.language.code, 'en')
+        self.assertEqual(merchant.currency.name, 'GBP')
+        self.assertEqual(merchant.payment_processor, 'gocoin')
+        self.assertEqual(merchant.verification_status, 'unverified')
