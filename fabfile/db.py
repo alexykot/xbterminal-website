9cdf9fea4d276cd87f4f567ff0de512e7a9f08e4
Kirill Goncharov <kdgoncharov@yandex.ru>
2015-08-24 12:44:13 +0300

    Update migration script

--- fabfile/db.py
+++ fabfile/db.py
@@ -4,5 +4,4 @@ from fabric.api import env, task, prefix
 @task
 def migrate():
     with prefix(". venv/bin/activate"):
-        env.run('python xbterminal/manage.py migrate oauth2_provider --fake')
         env.run('python xbterminal/manage.py migrate')
