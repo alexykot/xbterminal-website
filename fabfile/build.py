c762641a8a7dd142117f9da7ae9619c48ac3301b
Kirill Goncharov <kdgoncharov@yandex.ru>
2015-10-07 15:29:04 +0300

    Move dev requirements to a separate file

--- fabfile/build.py
+++ fabfile/build.py
@@ -1,39 +1,30 @@
-import re
-
 from fabric.api import env, task, settings, prefix, local, lcd
 
 
 @task
 def dirs():
     env.run("mkdir -p logs")
-    env.run("mkdir -p firmware")
     env.run("mkdir -p reports")
     env.run("chmod -R a+w reports")
     env.run("mkdir -p media")
     env.run("chmod -R a+w media")
 
 
-def pip_version():
-    if env.run == local:
-        output = env.run("pip --version", capture=True)
-    else:
-        output = env.run("pip --version")
-    m = re.search(r"pip\s(?P<version>[\d\.]+)\sfrom", output)
-    return m.group("version")
-
-
 @task
-def venv():
+def venv(development='yes'):
     with settings(warn_only=True):
-        result = env.run("test requirements.txt -ot venv/bin/activate")
+        test_command = 'test requirements.txt -ot venv/bin/activate'
+        if development == 'yes':
+            test_command += ' -a requirements_dev.txt -ot venv/bin/activate'
+        result = env.run(test_command)
     if result.failed:
-        env.run("virtualenv -p /usr/bin/python2 venv")
-        with prefix(". venv/bin/activate"):
-            pip_install = "pip install -r requirements.txt --upgrade"
-            if pip_version() > "1.4":
-                pip_install += " --allow-all-external --allow-unverified pyPdf"
-            env.run(pip_install)
-        env.run("touch venv/bin/activate")
+        env.run('virtualenv -p /usr/bin/python2 venv')
+        with prefix('. venv/bin/activate'):
+            if development == 'yes':
+                env.run('pip install -r requirements_dev.txt --upgrade')
+            else:
+                env.run('pip install -r requirements.txt --upgrade')
+        env.run('touch venv/bin/activate')
 
 
 @task
@@ -62,7 +53,7 @@ def compilemessages():
 
 
 @task(default=True)
-def all():
+def all(development='yes'):
     dirs()
-    venv()
+    venv(development=development)
     compilemessages()
--- fabfile/check.py
+++ fabfile/check.py
@@ -4,7 +4,7 @@ from fabric.api import task, prefix, local
 @task
 def python():
     with prefix('. venv/bin/activate'):
-        local('flake8 --max-line-length=140 fabfile')
+        local('flake8 fabfile')
         local('flake8 '
               '--exclude=migrations,paymentrequest_pb2.py '
               '--max-line-length=140 '
