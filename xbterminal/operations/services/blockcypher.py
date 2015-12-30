5be0f73f5bd012edf99c266b34a2d2d301c77c84
Kirill Goncharov <kdgoncharov@yandex.ru>
2015-12-30 13:54:53 +0300

    Add intergration with blockcypher

---
+++ xbterminal/operations/services/blockcypher.py
@@ -0,0 +1,23 @@
+import requests
+
+BLOCKCYPHER_CHAINS = {
+    'mainnet': 'main',
+    'testnet': 'test3',
+}
+BLOCKCYPHER_CONFIDENCE_THRESHOLD = 0.99
+
+
+def is_tx_reliable(tx_id, network):
+    """
+    http://dev.blockcypher.com/#transaction-confidence-endpoint
+    """
+    api_url = 'https://api.blockcypher.com/v1/btc/{chain}/txs/{tx_id}/confidence'
+    response = requests.get(api_url.format(
+        chain=BLOCKCYPHER_CHAINS[network],
+        tx_id=tx_id))
+    response.raise_for_status()
+    data = response.json()
+    if data['confidence'] >= BLOCKCYPHER_CONFIDENCE_THRESHOLD:
+        return True
+    else:
+        return False
