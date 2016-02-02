6b572415c06ee2ad833ff4174ed10a1e76012fbb
Kirill Goncharov <kdgoncharov@yandex.ru>
2016-02-02 15:26:17 +0300

    Fix E221 warnings

--- xbterminal/operations/__init__.py
+++ xbterminal/operations/__init__.py
@@ -5,6 +5,6 @@ from decimal import Decimal
 
 FIAT_DEC_PLACES = Decimal('0.00000000')
 FIAT_MIN_OUTPUT = Decimal('0.01000000')
-BTC_DEC_PLACES  = Decimal('0.00000000')
+BTC_DEC_PLACES = Decimal('0.00000000')
 BTC_DEFAULT_FEE = Decimal('0.00010000')
-BTC_MIN_OUTPUT  = Decimal('0.00005460')
+BTC_MIN_OUTPUT = Decimal('0.00005460')
