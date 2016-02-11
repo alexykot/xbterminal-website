50be112e07f0c5f8db9f9383ecb3d71659ef8a54
Kirill Goncharov <kdgoncharov@yandex.ru>
2016-02-11 15:34:52 +0300

    Update blockcypher module

--- xbterminal/operations/services/blockcypher.py
+++ xbterminal/operations/services/blockcypher.py
@@ -16,10 +16,11 @@ def is_tx_reliable(tx_id, network):
     """
     http://dev.blockcypher.com/#transaction-confidence-endpoint
     """
-    api_url = 'https://api.blockcypher.com/v1/btc/{chain}/txs/{tx_id}'
-    response = requests.get(api_url.format(
+    api_url = 'https://api.blockcypher.com/v1/btc/{chain}/txs/{tx_id}'.format(
         chain=BLOCKCYPHER_CHAINS[network],
-        tx_id=tx_id))
+        tx_id=tx_id)
+    payload = {'includeConfidence': 'true'}
+    response = requests.get(api_url, params=payload)
     response.raise_for_status()
     data = response.json()
     if data['confirmations'] >= 1:
