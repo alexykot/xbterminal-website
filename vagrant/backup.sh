5b554510012e6fb7d32ec80eb39d5ff79864b007
Kirill Goncharov <kdgoncharov@yandex.ru>
2016-02-14 21:36:11 +0300

    Update vagrant backup script

--- vagrant/backup.sh
+++ vagrant/backup.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+set -e
+
+mkdir -p /vagrant/vagrant/backup
 pg_dump -Fc -U postgres xbt > /vagrant/vagrant/backup/xbt.dump
 cp /var/lib/bitcoind/testnet3/wallet.dat /vagrant/vagrant/backup/
