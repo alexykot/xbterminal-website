e08d7260a3f5f1ee2a131a58274c330cf4c279b4
Kirill Goncharov <kdgoncharov@yandex.ru>
2016-05-13 03:34:39 +0300

    Create function for checking managed accounts

--- xbterminal/website/tests/test_utils.py
+++ xbterminal/website/tests/test_utils.py
@@ -2,10 +2,36 @@ from django.conf import settings
 from django.core import mail
 from django.test import TestCase
 
+from website.utils.accounts import check_managed_accounts
 from website.utils.email import send_error_message
+from website.tests.factories import (
+    MerchantAccountFactory,
+    AccountFactory)
 from operations.tests.factories import (
     PaymentOrderFactory,
     WithdrawalOrderFactory)
+
+
+class AccountsUtilsTestCase(TestCase):
+
+    def test_check_managed_accounts(self):
+        merchant = MerchantAccountFactory.create()
+        self.assertFalse(check_managed_accounts(merchant))
+        AccountFactory.create(
+            merchant=merchant,
+            currency__name='GBP',
+            instantfiat_merchant_id='test')
+        self.assertFalse(check_managed_accounts(merchant))
+        AccountFactory.create(
+            merchant=merchant,
+            currency__name='USD',
+            instantfiat_merchant_id='test')
+        self.assertFalse(check_managed_accounts(merchant))
+        AccountFactory.create(
+            merchant=merchant,
+            currency__name='EUR',
+            instantfiat_merchant_id='test')
+        self.assertTrue(check_managed_accounts(merchant))
 
 
 class EmailUtilsTestCase(TestCase):
