8c4902ffef38521d15eab62934fd8cffe8587a06
Kirill Goncharov <kdgoncharov@yandex.ru>
2016-05-19 17:33:54 +0300

    Remove transactions with zero amount

---
+++ xbterminal/website/migrations/0057_data_rm_zero_tx.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+def remove_zero_tx(apps, schema_editor):
+    Transaction = apps.get_model('website', 'Transaction')
+    for transaction in Transaction.objects.all():
+        if not transaction.amount:
+            transaction.delete()
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('website', '0056_schema_document_types'),
+    ]
+
+    operations = [
+        migrations.RunPython(remove_zero_tx,
+                             migrations.RunPython.noop),
+    ]
