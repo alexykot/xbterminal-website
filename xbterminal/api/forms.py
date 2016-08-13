6fb7625d31f0b3036ccaad9ba932e8839d42abfb
Kirill Goncharov <kdgoncharov@yandex.ru>
2016-08-13 19:54:51 +0300

    Remove old withdrawal form

--- xbterminal/api/forms.py
+++ xbterminal/api/forms.py
@@ -2,8 +2,6 @@ from decimal import Decimal
 from django import forms
 from django.core.validators import RegexValidator
 
-from website.models import Device
-
 
 class PaymentForm(forms.Form):
 
@@ -17,24 +15,3 @@ class PaymentForm(forms.Form):
         required=False,
         validators=[RegexValidator('^[0-9a-fA-F:]{17}$')])
     qr_code = forms.BooleanField(required=False)
-
-
-class WithdrawalForm(forms.Form):
-
-    device = forms.CharField()
-    amount = forms.DecimalField(
-        max_digits=12,
-        decimal_places=2,
-        min_value=Decimal('0.01'))
-
-    def clean_device(self):
-        try:
-            return Device.objects.get(key=self.cleaned_data['device'],
-                                      status='active')
-        except Device.DoesNotExist:
-            raise forms.ValidationError('Invalid device key')
-
-    @property
-    def error_message(self):
-        for field, errors in self.errors.items():
-            return '{0} - {1}'.format(field, errors[0]).capitalize()
