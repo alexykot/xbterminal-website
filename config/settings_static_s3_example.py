0fab8415856563970475de48027174ba3993ee21
Kirill Goncharov <kdgoncharov@yandex.ru>
2016-08-15 00:09:23 +0300

    Update example of settings for static deployment

--- config/settings_static_s3_example.py
+++ config/settings_static_s3_example.py
@@ -1,16 +1,10 @@
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-
 # xbt-webstatic-prod, xbt-webstatic-stage
 AWS_STORAGE_BUCKET_NAME = '#AWS_STORAGE_BUCKET_NAME#'
 AWS_ACCESS_KEY_ID = '#AWS_ACCESS_KEY_ID#'
 AWS_SECRET_ACCESS_KEY = '#AWS_SECRET_ACCESS_KEY#'
+AWS_DEFAULT_ACL = 'public'
 
 AWS_S3_CUSTOM_DOMAIN = '%s.s3-eu-west-1.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-STATIC_URL = "https://%s/static/" % AWS_S3_CUSTOM_DOMAIN
+STATIC_URL = 'https://%s/' % AWS_S3_CUSTOM_DOMAIN
 
 STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-
-
-RECAPTCHA_PUBLIC_KEY = '#RECAPTCHA_PUBLIC_KEY#'
-RECAPTCHA_PRIVATE_KEY = '#RECAPTCHA_PRIVATE_KEY#'
