55bc1efe2e74d4a62ec836263018696800da730e
Kirill Goncharov <kdgoncharov@yandex.ru>
2016-08-30 23:29:17 +0300

    Use aptly API to find latest versions of packages

--- xbterminal/api/tests/test_aptly.py
+++ xbterminal/api/tests/test_aptly.py
@@ -17,17 +17,14 @@ class AptlyTestCase(TestCase):
 
     @patch('api.utils.aptly.requests.get')
     def test_get_latest_version(self, get_mock):
-        get_mock.return_value = Mock(
-            content='Package: xbterminal-firmware\n'
-                    'Version: 0.9.1\n'
-                    '\n'
-                    'Package: xbterminal-firmware\n'
-                    'Version: 0.9.2\n'
-                    '\n'
-                    'Package: test\n'
-                    'Version: 1.0.0\n')
-        latest = aptly.get_latest_version('qemuarm', 'xbterminal-firmware')
-        self.assertEqual(latest, '0.9.2')
+        get_mock.return_value = Mock(**{
+            'json.return_value': [{
+                'Version': '0.9.6.20160822-b1-master-r0.0',
+            }],
+        })
+        latest = aptly.get_latest_version('qemuarm', 'xbterminal-gui')
+        self.assertEqual(latest, '0.9.6.20160822-b1-master-r0.0')
         self.assertEqual(
             get_mock.call_args[0][0],
-            'http://test/repos/deb/jethro/xbtfw-qemuarm-dev/dists/poky/main/binary-armel/Packages')
+            'https://test/api/repos/xbtfw-qemuarm-dev/packages'
+            '?q=xbterminal-gui&format=details')
