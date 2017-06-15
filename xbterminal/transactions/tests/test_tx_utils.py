726f1919c89ea726f77faf2896843616cb634761
Kirill Goncharov <kdgoncharov@yandex.ru>
2017-06-15 14:49:39 +0300

    Create module with methods for constructing and signing bitcoin transactions

---
+++ xbterminal/transactions/tests/test_tx_utils.py
@@ -0,0 +1,48 @@
+from decimal import Decimal
+
+from django.test import TestCase
+
+from pycoin.key.BIP32Node import BIP32Node
+
+from transactions.utils.tx import create_tx
+
+
+class TxUtilsTestCase(TestCase):
+
+    def test_create_tx(self):
+        tx_inputs = [{
+            'txid': '81ce5b064d49224c27ac8256e13b6964'
+                    'b71e0be59ee516dc9ccf303c39712273',
+            'vout': 1,
+            'amount': Decimal('0.005'),
+            'scriptPubKey': '76a914b9dbfb58fddf964f6ab'
+                            '8146e949a370850bc629a88ac',
+            'private_key': BIP32Node.from_hwif(
+                'tprv8km9JkJsMthQbmJqG9YgA23C1shqTFQy'
+                'GcJK9BTsWjxyRdRVG3kHCPgcNtFEZSW9RM6D'
+                'et955ePiQLRW2FTPVzsAQRPfoe4XGPq9ACLA7iw'),
+        }]
+        tx_outputs = {'n2p8YJojY7zewwt4ngonYUPgkV1xy7qfjR': Decimal('0.004')}
+        tx = create_tx(tx_inputs, tx_outputs)
+        self.assertIsNone(tx.check())
+        self.assertIs(tx.missing_unspents(), False)
+        self.assertEqual(tx.bad_signature_count(), 0)
+        self.assertEqual(len(tx.id()), 64)
+        self.assertEqual(tx.fee(), 100000)
+
+    def test_create_tx_bad_signature(self):
+        tx_inputs = [{
+            'txid': '81ce5b064d49224c27ac8256e13b6964'
+                    'b71e0be59ee516dc9ccf303c39712273',
+            'vout': 1,
+            'amount': Decimal('0.005'),
+            'scriptPubKey': '76a914b9dbfb58fddf964f6ab'
+                            '8146e949a370850bc629a88ac',
+            'private_key': BIP32Node.from_hwif(
+                'tprv8oMYy2BFqU2vWV6U9h6yDxSLfYXUKrvC'
+                'dGDnKP365utwPc7cfubKPWkjSdxCsWAFArQj'
+                'JkV4Eg5yJWELnEarzUyYcY3rZKZTNqHRU6JNN9H'),
+        }]
+        tx_outputs = {'n2p8YJojY7zewwt4ngonYUPgkV1xy7qfjR': Decimal('0.004')}
+        with self.assertRaises(ValueError):
+            create_tx(tx_inputs, tx_outputs)
---
+++ xbterminal/transactions/utils/tx.py
@@ -0,0 +1,40 @@
+from bitcoin.core import COIN
+from pycoin.tx.Spendable import Spendable
+from pycoin.tx.Tx import Tx
+from pycoin.tx.TxOut import TxOut
+from pycoin.tx.pay_to import build_hash160_lookup
+from pycoin.ui import standard_tx_out_script
+
+
+def create_tx(tx_inputs, tx_outputs):
+    """
+    Accepts:
+        tx_inputs: list of dicts containing inputs data
+        tx_outputs: dict containing addresses and corresponding amounts
+    Returns:
+        signed transaction (Tx instance)
+    """
+    spendables = []
+    private_keys = []
+    for tx_input in tx_inputs:
+        spendables.append(Spendable.from_dict({
+            'tx_hash_hex': tx_input['txid'],
+            'tx_out_index': tx_input['vout'],
+            'coin_value': int(tx_input['amount'] * COIN),
+            'script_hex': tx_input['scriptPubKey'],
+        }))
+        private_keys.append(tx_input['private_key'])
+    txs_in = [spendable.tx_in() for spendable in spendables]
+    txs_out = []
+    for address, amount in tx_outputs.items():
+        script = standard_tx_out_script(address)
+        out = TxOut(int(amount * COIN), script)
+        txs_out.append(out)
+    tx = Tx(version=1, txs_in=txs_in, txs_out=txs_out, lock_time=0)
+    tx.set_unspents(spendables)
+    hash160_lookup = build_hash160_lookup(
+        [key.secret_exponent() for key in private_keys])
+    tx.sign(hash160_lookup)
+    if tx.bad_signature_count() != 0:
+        raise ValueError('Invalid signature')
+    return tx
