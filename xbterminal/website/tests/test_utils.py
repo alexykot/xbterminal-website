a288e2e8e263850c7075ed3471e349073610e7e0
Kirill Goncharov <kdgoncharov@yandex.ru>
2017-10-04 16:17:28 +0300

    Remove utils.accounts module from website app

--- xbterminal/website/tests/test_utils.py
+++ xbterminal/website/tests/test_utils.py
@@ -1,4 +1,3 @@
-from decimal import Decimal
 import unicodecsv
 
 from mock import patch, Mock
@@ -6,13 +5,7 @@ from django.conf import settings
 from django.core import mail
 from django.test import TestCase
 
-from website.models import (
-    Device,
-    INSTANTFIAT_PROVIDERS,
-    KYC_DOCUMENT_TYPES)
-from website.utils.accounts import (
-    update_managed_accounts,
-    update_balances)
+from website.models import Device, KYC_DOCUMENT_TYPES
 from website.utils.devices import get_device_info, MAIN_PACKAGES
 from website.utils.kyc import upload_documents
 from website.utils.files import encode_base64, decode_base64
@@ -22,90 +15,8 @@ from website.utils.reports import (
 from website.tests.factories import (
     MerchantAccountFactory,
     KYCDocumentFactory,
-    AccountFactory,
-    TransactionFactory,
     DeviceFactory)
-from operations.tests.factories import (
-    PaymentOrderFactory,
-    WithdrawalOrderFactory)
 from transactions.tests.factories import BalanceChangeFactory
-
-
-class AccountsUtilsTestCase(TestCase):
-
-    @patch('website.utils.accounts.cryptopay.list_accounts')
-    def test_update_managed_accounts(self, list_mock):
-        list_mock.return_value = [
-            {'id': 'a1', 'currency': 'BTC'},
-            {'id': 'a2', 'currency': 'GBP'},
-            {'id': 'a3', 'currency': 'USD'},
-            {'id': 'a4', 'currency': 'EUR'},
-        ]
-        merchant = MerchantAccountFactory.create(
-            instantfiat_provider=INSTANTFIAT_PROVIDERS.CRYPTOPAY,
-            instantfiat_api_key='test-key')
-        AccountFactory.create(merchant=merchant,
-                              currency__name='EUR',
-                              instantfiat=True,
-                              instantfiat_account_id='test')
-        update_managed_accounts(merchant)
-        self.assertEqual(merchant.account_set.count(), 3)
-        self.assertFalse(merchant.account_set.filter(
-            currency__name='BTC', instantfiat=True).exists())
-        account_gbp = merchant.account_set.get(currency__name='GBP',
-                                               instantfiat=True)
-        self.assertEqual(account_gbp.instantfiat_account_id, 'a2')
-        account_usd = merchant.account_set.get(currency__name='USD',
-                                               instantfiat=True)
-        self.assertEqual(account_usd.instantfiat_account_id, 'a3')
-        account_eur = merchant.account_set.get(currency__name='EUR',
-                                               instantfiat=True)
-        self.assertEqual(account_eur.instantfiat_account_id, 'a4')
-
-    @patch('website.utils.accounts.cryptopay.list_transactions')
-    def test_update_balances(self, list_mock):
-        merchant = MerchantAccountFactory.create(
-            instantfiat_provider=INSTANTFIAT_PROVIDERS.CRYPTOPAY,
-            instantfiat_api_key='test-key')
-        account = AccountFactory.create(merchant=merchant,
-                                        currency=merchant.currency,
-                                        instantfiat=True,
-                                        instantfiat_account_id='test-id')
-        payment_tx = TransactionFactory.create(
-            payment=PaymentOrderFactory.create(
-                instantfiat_invoice_id='00bb555b-3ebc-4476-8aa8-214e28aa6c03'),
-            account=account,
-            amount=Decimal('0.55'))
-        withdrawal_tx = TransactionFactory.create(
-            withdrawal=WithdrawalOrderFactory.create(
-                instantfiat_reference='BT95281015'),
-            account=account,
-            amount=Decimal('-0.35'))
-        TransactionFactory.create(
-            account=account,
-            instantfiat_tx_id='115',
-            amount=Decimal('1.15'))
-        self.assertEqual(account.balance_, Decimal('1.35'))
-        list_mock.return_value = [
-            {'id': 110, 'amount': 0.55, 'type': 'Invoice',
-             'description': 'Order 00bb555b-3ebc-4476-8aa8-214e28aa6c03'},
-            {'id': 112, 'amount': -0.35, 'type': 'Bitcoin payment',
-             'reference': 'BT95281015'},
-            {'id': 115, 'amount': 1.15, 'type': 'Card reload'},
-            {'id': 117, 'amount': 0.65, 'type': 'Invoice',
-             'description': 'Order 140a2e0d-07ce-4bdf-9ea3-91a774261355'},
-            {'id': 120, 'amount': -0.85, 'type': 'Bitcoin payment',
-             'reference': 'BT120200116'},
-        ]
-        update_balances(merchant)
-        self.assertEqual(list_mock.call_args[0][0], 'test-id')
-        self.assertEqual(list_mock.call_args[0][1], 'test-key')
-        self.assertEqual(account.transaction_set.count(), 5)
-        self.assertEqual(account.balance_, Decimal('1.15'))
-        payment_tx.refresh_from_db()
-        self.assertEqual(payment_tx.instantfiat_tx_id, '110')
-        withdrawal_tx.refresh_from_db()
-        self.assertEqual(withdrawal_tx.instantfiat_tx_id, '112')
 
 
 class DeviceUtilsTestCase(TestCase):
