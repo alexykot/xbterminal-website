59f1a512aa3f0729d3b1ef4aeb254ae769f4864b
Kirill Goncharov <kdgoncharov@yandex.ru>
2017-10-05 16:46:46 +0300

    Define COINS enum

--- xbterminal/wallet/constants.py
+++ xbterminal/wallet/constants.py
@@ -1,12 +1,21 @@
 from django.utils.translation import ugettext_lazy as _
 
+from enum import Enum
 from extended_choices import Choices
+
+
+class COINS(Enum):
+    BTC = ('BTC', 0, _('Bitcoin'))
+    TBTC = ('XTN', 1, _('Bitcoin Testnet'))
+
+    def __init__(self, pycoin_code, bip44_type, display_name):
+        self.pycoin_code = pycoin_code
+        self.bip44_type = bip44_type
+        self.display_name = display_name
+
 
 BIP44_PURPOSE = 0
 
-BIP44_COIN_TYPES = Choices(
-    ('BTC', 0, _('Bitcoin')),
-    ('XTN', 1, _('Bitcoin Testnet')),
-)
+BIP44_COIN_TYPES = Choices(*(item.value for item in COINS))
 
 MAX_INDEX = 2 ** 30
