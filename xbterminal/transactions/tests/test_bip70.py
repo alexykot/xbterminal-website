f4f54604cfb9691fdffe558fc19840d529be61e0
Kirill Goncharov <kdgoncharov@yandex.ru>
2017-10-07 12:59:20 +0300

    Add test for bip70.create_payment_request function

--- xbterminal/transactions/tests/test_bip70.py
+++ xbterminal/transactions/tests/test_bip70.py
@@ -1,18 +1,52 @@
 import binascii
+import datetime
+from decimal import Decimal
 
 from django.test import TestCase
+from django.utils import timezone
 
 import bitcoin
 from bitcoin.core import b2lx
 
+from transactions.utils.paymentrequest_pb2 import PaymentRequest
 from transactions.utils.bip70 import (
+    create_payment_request,
     parse_payment)
 
 
 class BIP70UtilsTestCase(TestCase):
 
+    def test_create_payment_request(self):
+        coin_name = 'TBTC'
+        deposit_address = 'miKz8WitnNacGewVxfadE8jKFzpoHjkWeU'
+        deposit_amount = Decimal('0.001')
+        created_at = datetime.datetime(2017, 10, 7, 9, 12, 36,
+                                       tzinfo=timezone.utc)
+        expires_at = datetime.datetime(2017, 10, 7, 9, 27, 36,
+                                       tzinfo=timezone.utc)
+        response_url = 'http://test'
+        memo = 'test'
+        expected_payment_details = binascii.unhexlify(
+            '0a0474657374121f08a08d06121976a9141ed5557f4914da4362c7bb'
+            '25be78ae9386b4e76d88ac1884b5e2ce052088bce2ce052a04746573'
+            '74320b687474703a2f2f74657374')
+        payment_request = create_payment_request(
+            coin_name,
+            [(deposit_address, deposit_amount)],
+            created_at,
+            expires_at,
+            response_url,
+            memo)
+
+        payment_request_obj = PaymentRequest()
+        payment_request_obj.ParseFromString(payment_request)
+        self.assertEqual(payment_request_obj.serialized_payment_details,
+                         expected_payment_details)
+        self.assertEqual(payment_request_obj.pki_type, 'x509+sha256')
+        self.assertIsNotNone(payment_request_obj.pki_data)
+        self.assertIsNotNone(payment_request_obj.signature)
+
     def test_parse_payment(self):
-        bitcoin.SelectParams('testnet')
         payment_message = binascii.unhexlify(
             '12e101010000000177b925097140496565508aa20440761091bcbcaa'
             'c352882988563d94bbd6a90f010000006a47304402206533bd1d0eb3'
@@ -38,6 +72,7 @@ class BIP70UtilsTestCase(TestCase):
             '00000000001976a9141ed5557f4914da4362c7bb25be78ae9386b4e7'
             '6d88ac000000001a1f08ac8a02121976a914063ce37eef0c8b22b6ee'
             '8b68fd22b1bbc0f9d4b088ac120361636b')
+        bitcoin.SelectParams('testnet')
         transactions, refund_addresses, payment_ack = \
             parse_payment(payment_message)
 
